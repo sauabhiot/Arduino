bbfd35ee779e1c680559e1bfa46a8003af23000e
saurabhiot <saurabh.iotcareer@gmail.com>
2025-08-14 16:51:41 +0530

    Circle splitting

--- XYPlotter/XYPlotter.ino
+++ XYPlotter/XYPlotter.ino
@@ -12,7 +12,7 @@
 #define clock_prescalar 1
 #define timer_frequency base_frequency/clock_prescalar
 
-#define total_gcodes 6
+#define total_gcodes 3
 
 double count=0.0;
 long times=0;
@@ -29,7 +29,7 @@ volatile double time_increment = 0.0;
 char *gcodes[total_gcodes] = {
   "G01 X100 Y100 F1000",
   "G01 X150 Y100 F1000",
-  "G02 X100 Y150 I-50 J0",
+  "G03 X100 Y150 I-50 J0",
   
 };
 */
@@ -39,7 +39,7 @@ char *gcodes[total_gcodes] = {
 char *gcodes[total_gcodes] = {
   "G01 X100 Y100 F1000",
   "G01 X100 Y150 F1000",
-  "G02 X50 Y100 I0 J-50",
+  "G03 X50 Y100 I0 J-50",
   
 };
 */
@@ -50,18 +50,18 @@ char *gcodes[total_gcodes] = {
 char *gcodes[total_gcodes] = {
   "G01 X100 Y100 F1000",
   "G01 X150 Y100 F1000",
-  "G02 X100 Y150 I-50 J0",
-  "G02 X50 Y100 I0 J-50",
+  "G03 X100 Y150 I-50 J0",
+  "G03 X50 Y100 I0 J-50",
 };
-
 */
+
 
 /*
 // Anti clockwise 180 - 270
 char *gcodes[total_gcodes] = {
   "G01 X100 Y100 F1000",
   "G01 X50 Y100 F1000",
-  "G02 X100 Y50 I50 J0",
+  "G03 X100 Y50 I50 J0",
   
 };
 
@@ -72,25 +72,55 @@ char *gcodes[total_gcodes] = {
 char *gcodes[total_gcodes] = {
   "G01 X100 Y100 F1000",
   "G01 X100 Y50 F1000",
-  "G02 X150 Y100 I0 J50",
+  "G03 X150 Y100 I0 J50",
   
 };
 */
 
+/*
 // Anti clockwise Full circle 0 - 360
 
 char *gcodes[total_gcodes] = {
   "G01 X100 Y100 F1000",
   "G01 X150 Y100 F1000",
-  "G02 X100 Y150 I-50 J0",
-  "G02 X50 Y100 I0 J-50",
-  "G02 X100 Y50 I50 J0",
-  "G02 X150 Y100 I0 J50",
+  "G03 X100 Y150 I-50 J0",
+  "G03 X50 Y100 I0 J-50",
+  "G03 X100 Y50 I50 J0",
+  "G03 X150 Y100 I0 J50",
+};
+
+*/
+
+
+// From 340 to 20 Anti clockwise
+char *gcodes[total_gcodes] = {
+   "G01 X100 Y100 F1000",
+   "G01 X146.61 Y82.81 F1000",
+   "G03 X146.66 Y117.03 I-46 J18",
 };
 
 
+/*
+// Equilateral Triange of 5 cm
+char *gcodes[total_gcodes] = {
+  "G01 X100 Y100 F1000",
+  "G01 X150 Y100",
+  "G01 X125 Y143.3",
+  "G01 X100 Y100",
+};
+*/
+
+/* // Square
+char *gcodes[total_gcodes] = {
+  "G01 X50 Y0 F1000",
+  "G01 X50 Y50",
+  "G01 X0 Y50",
+  "G01 X0 Y0"
+};
+*/
 int c=1;
-int gcode_indx=0,prev_gcode_indx=0;
+int gcode_indx = 0 , prev_gcode_indx = 0;
+int total_gcode_sub_indx = 0 , gcode_sub_indx = 0 , prev_gcode_sub_indx = 0;
 volatile double x=0,y=0,e=0,f=0;
 volatile double prev_x=0;
 volatile double prev_y=0;
@@ -103,11 +133,12 @@ double dX = 0.0;
 double p = 0.0;
 int x_primary=0;
 
-double ins_x=0, ins_y=0;
-double p_x = 0,p_y = 0;
+double ins_x = 0, ins_y = 0;
+double p_x = 0, p_y = 0;
+double gcode_step_x = 0, gcode_step_y = 0;
 
 double formula_denominator;
-int linear_movement=1;
+int g_code=1;
 
 double chord_length=0.0;
 double arc_length=0.0;
@@ -116,10 +147,39 @@ double I=0.0,J=0.0;
 double arc_center_x=0;
 double arc_center_y=0;
 double theta;
-int x_decrease=0;
-int y_decrease=0;
+int x_previouse=0;
+int y_previous=0;
 
 long count2=0;
+
+int current_quadrant = 1, start_quadrant = 0, end_quadrant = 0;
+
+int clockwise = 0;
+
+double q1_x1_limit = 0;
+double q1_y1_limit = 0;
+
+double q1_x2_limit = 0;
+double q1_y2_limit = 0;
+
+double q2_x1_limit = 0;
+double q2_y1_limit = 0;
+
+double q2_x2_limit = 0;
+double q2_y2_limit = 0;
+
+
+double q3_x1_limit = 0;
+double q3_y1_limit = 0;
+
+double q3_x2_limit = 0;
+double q3_y2_limit = 0;
+
+double q4_x1_limit = 0;
+double q4_y1_limit = 0;
+
+double q4_x2_limit = 0;
+double q4_y2_limit = 0;
 
 
 void delay_1us_nop() {
@@ -134,33 +194,40 @@ ISR(TIMER1_COMPA_vect){
   if(travelled>distance){
     TCCR1B &= ~((1 << CS12) | (1 << CS11) | (1 << CS10)); 
     if(gcode_indx<total_gcodes-1) gcode_indx++;
-    Serial.println(travelled);
-    Serial.println(p_x);
-    Serial.println(p_y);
+    //Serial.println(travelled);
+    //Serial.println(p_x);
+    //Serial.println(p_y);
     //Serial.println(count2);
+    
     
     travelled=0.0;
     ins_x=0.0;
     ins_y=0.0;
+    gcode_step_x = 0.0;
+    gcode_step_y = 0.0;
     PORTB |= (1 << 0);
   }else{
     //Serial.println(get_direction_x());
     travelled = travelled + mm_per_step;
     PORTB &= ~(1<<0);  
-    if(linear_movement==1){
-      ins_x = travelled/formula_denominator;
-      ins_y = slope * ins_x;
-      //Serial.println(ins_y,DEC);
+    if(g_code==1){
+      ins_x =  (travelled/formula_denominator);
+      ins_y =    ((slope * (ins_x )));
+    
+      
       if(abs(ins_x)>mm_per_step){      
-        if(abs(abs(ins_x) - p_x) >= mm_per_step){
-          p_x = p_x + mm_per_step*get_direction_x();
+        if(abs(ins_x - gcode_step_x) >= mm_per_step){
+          p_x = p_x + mm_per_step * get_direction_x();
+          gcode_step_x = gcode_step_x + (mm_per_step  * get_direction_x());
           step_x();
         }
       }
 
+    
       if(abs(ins_y)>mm_per_step){
-        if(abs(abs(ins_y) - p_y) >= mm_per_step){
+        if(abs(ins_y - gcode_step_y) >= mm_per_step){
           p_y = p_y + mm_per_step*get_direction_y();
+          gcode_step_y = gcode_step_y + (mm_per_step * get_direction_y());
           step_y();
         }
       }
@@ -222,39 +289,129 @@ void step_y(){
     PORTD &= ~(1 << 3);
 }
 
+
+void set_quadrants(){
+  q1_x1_limit = (arc_center_x + radius);
+  q1_y1_limit = (arc_center_y);
+
+  q1_x2_limit = (arc_center_x);
+  q1_y2_limit = (arc_center_y + radius);
+
+  q2_x1_limit = (arc_center_x);
+  q2_y1_limit = (arc_center_y + radius);
+
+  q2_x2_limit = (arc_center_x - radius);
+  q2_y2_limit = (arc_center_y);
+
+
+  q3_x1_limit = (arc_center_x - radius);
+  q3_y1_limit = (arc_center_y);
+
+  q3_x2_limit = (arc_center_x);
+  q3_y2_limit = (arc_center_y - radius);
+
+  q4_x1_limit = (arc_center_x);
+  q4_y1_limit = (arc_center_y - radius);
+
+  q4_x2_limit = (arc_center_x + radius);
+  q4_y2_limit = (arc_center_y);
+
+if((q1_x1_limit > prev_x) && (prev_x > q1_x2_limit) && (q1_y1_limit < prev_y) && (prev_y < q1_y2_limit)){
+  start_quadrant = 1;
+}else if((q2_x1_limit > prev_x) && (prev_x > q2_x2_limit) && (q2_y1_limit > prev_y) && (prev_y > q2_y2_limit)){
+  start_quadrant = 2;
+}else if((q3_x1_limit < prev_x) && (prev_x < q3_x2_limit) && (q3_y1_limit > prev_y) && (prev_y > q3_y2_limit)){
+  start_quadrant = 3;
+}else if((q4_x1_limit < prev_x) && (prev_x < q4_x2_limit) && (q4_y1_limit < prev_y) && (prev_y < q4_y2_limit)){
+  start_quadrant = 4;
+}
+
+
+if((q1_x1_limit > x) && (x > q1_x2_limit) && (q1_y1_limit < y) && (y < q1_y2_limit)){
+  end_quadrant = 1;
+}else if((q2_x1_limit > x) && (x > q2_x2_limit) && (q2_y1_limit > y) && (y > q2_y2_limit)){
+  end_quadrant = 2;
+}else if((q3_x1_limit < x) && (x < q3_x2_limit) && (q3_y1_limit > y) && (y > q3_y2_limit)){
+  end_quadrant = 3;
+}else if((q4_x1_limit < x) && (x < q4_x2_limit) && (q4_y1_limit < y) && (y < q4_y2_limit)){
+  end_quadrant = 4;
+}
+
+//Serial.println(start_quadrant);
+//Serial.println(end_quadrant);
+  int start_indx = (start_quadrant== 4) ? 0 : start_quadrant;
+  int end_indx = end_quadrant;
+  total_gcode_sub_indx = (end_indx - start_indx) + 1;
+  current_quadrant = start_quadrant;
+  //Serial.println(total_gcode_sub_indx);
+}
+
 void set_duration(){
   
-  if(linear_movement){
+  if(g_code == 1){
     double c_x = x - p_x;
     double c_y = y - p_y;
     distance = sqrt((c_x * c_x) + (c_y * c_y));
     duration = distance/f;
     dY = (y-prev_y);
-    dX =  (x-prev_x)==0?0.00001:(x-prev_x);
+    dX =  (x-prev_x)==0?0.0000001:(x-prev_x);
     slope = ((double)dY/dX);
     formula_denominator =sqrt(1+slope*slope);
-    //Serial.println(slope);
+    //Serial.println(distance,DEC);
   }else{
-    //Serial.println(linear_movement);
+    clockwise = (g_code==2) ? 1 : 0;
     arc_center_x = I + prev_x;
     arc_center_y = J + prev_y;
     //Serial.println(arc_center_x);
     //Serial.println(arc_center_y);
     radius = sqrt(pow(prev_x - arc_center_x,2) + pow(prev_y - arc_center_y,2));  
-    
-    chord_length = sqrt(pow(prev_x-x,2) + pow(prev_y-y,2));
-    
-    theta = (asin(chord_length/(2*radius)))*2;
-    //Serial.println(theta);
-    
-    arc_length = theta * radius;
-    //Serial.println(arc_length);
-    distance = arc_length;
-    
-    set_direction();
-    //Serial.println(duration);
+    set_quadrants();
+    process_quadrant_arc();
   }
-  //Serial.println(duration);
+}
+
+void next_quadrant(){
+  current_quadrant = current_quadrant ==4?1:current_quadrant++;
+
+}
+
+void process_quadrant_arc(){
+  int intermedite_x,intermediate_y;
+  if(current_quadrant != end_quadrant){
+    if(current_quadrant == start_quadrant){
+        switch (current_quadrant) {
+          case 1:
+            intermedite_x = clockwise ? q1_x1_limit : q1_x2_limit;
+            intermedite_y = clockwise ? q1_y1_limit : q1_y2_limit;
+          break;
+          case 2:
+            intermedite_x = clockwise ? q2_x1_limit : q2_x2_limit;
+            intermedite_y = clockwise ? q2_y1_limit : q2_y2_limit;
+          break;
+          case 3:
+            intermedite_x = clockwise ? q3_x1_limit : q3_x2_limit;
+            intermedite_y = clockwise ? q3_y1_limit : q3_y2_limit;
+          break;
+          case 4:
+            intermedite_x = clockwise ? q4_x1_limit : q4_x2_limit;
+            intermedite_y = clockwise ? q4_y1_limit : q4_y2_limit;
+          break;
+
+        }
+    }
+    
+  }
+  set_sub_circular_motion_params();
+}
+
+
+
+void set_sub_circular_motion_params(){
+    chord_length = sqrt(pow(prev_x-x,2) + pow(prev_y-y,2));
+    theta = (asin(chord_length/(2*radius)))*2;
+    arc_length = theta * radius;
+    distance = arc_length;
+    set_direction();
 }
 
 void set_target_freq(){
@@ -337,7 +494,7 @@ void parse_gcodes(int indx){
     char* val= token+1;
     switch(type){
       case 'G':
-        c=atoi(val);
+        g_code=atoi(val);
         break;
       case 'X':
         prev_x=x;
@@ -364,12 +521,19 @@ void parse_gcodes(int indx){
     
   }
 
-  linear_movement = (c==1);
-  set_duration();  
+  //Serial.println(indx);
   set_target_freq();
-  set_direction(); 
+  set_duration();  
+  
 }
 
+void printXY(double c_x, double c_y, double r, double angle){
+  double x = c_x + r * cos(angle);
+  double y = c_y + r * sin(angle);
+  Serial.println(x,DEC);
+  Serial.println(y,DEC);
+
+}
 
 void setup() {
   Serial.begin(115200);
@@ -401,6 +565,9 @@ void loop() {
     parse_gcodes(gcode_indx);
     prev_gcode_indx=gcode_indx;
     TCCR1B = (1<<WGM12) | (1<<CS10);
+  }
+  if(gcode_sub_indx>prev_gcode_sub_indx){
+
   }
 }
 
